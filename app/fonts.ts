3b0c161811bf49d35aa2d16723bde4d6b6701ceb
Trevor Cash <trevorcash.cs@gmail.com>
2026-07-03 17:13:34 -0400

    chore: removes unused exports

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,11 +1,11 @@
 import { Inter, IBM_Plex_Mono } from "next/font/google";
 
-export const inter = Inter({
+const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-export const ibmPlexMono = IBM_Plex_Mono({
+const ibmPlexMono = IBM_Plex_Mono({
   variable: "--font-ibm-plex-mono",
   subsets: ["latin"],
   weight: ["400", "500", "600"],
