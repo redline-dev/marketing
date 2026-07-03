48d04d262766da1bb1287ec4a14c51916735cf57
Trevor Cash <trevorcash.cs@gmail.com>
2026-07-03 17:14:18 -0400

    chore: runs prettier write

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,6 @@
 import type { Preview } from "@storybook/nextjs-vite";
-import '../app/globals.css';
-import './fonts.css';
+import "../app/globals.css";
+import "./fonts.css";
 
 const preview: Preview = {
   parameters: {
